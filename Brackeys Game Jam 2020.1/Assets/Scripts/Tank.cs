727cc903ae37715aee010bede785359e8c024c70
3cki <ecki_cool@gmx.de>
2020-02-16 12:08:04 +0100

    added models and waypoints

---
+++ Brackeys Game Jam 2020.1/Assets/Scripts/Tank.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour
+{
+    public GameObject track;
+
+    private List<Vector2> waypoints;
+
+    void Start()
+    {
+        // create list of waypoints the tank has to pass
+        if (track == null)
+            track = GameObject.Find("Track");
+
+        waypoints = new List<Vector2>();
+
+        foreach (Transform child in track.transform) {
+            waypoints.Add(new Vector2(child.position.x, child.position.z));
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
