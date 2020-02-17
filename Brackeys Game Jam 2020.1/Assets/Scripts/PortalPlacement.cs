861d27ce95f2e1a66671ced1d6bacb11605118c6
3cki <ecki_cool@gmx.de>
2020-02-17 13:36:36 +0100

    added basic portal functionality

---
+++ Brackeys Game Jam 2020.1/Assets/Scripts/PortalPlacement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalPlacement : MonoBehaviour
+{
+    public LayerMask clickMask;
+    public GameObject portalPreview, leftPortal, rightPortal;
+
+    private Vector3 clickPosition;
+
+    private void Start() {
+        leftPortal.SetActive(false);
+        rightPortal.SetActive(false);
+    }
+
+    private void Update() {
+        positionPortalPreview();
+        placePortal();
+    }
+
+    void positionPortalPreview() {
+        clickPosition = -Vector3.one;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 300f, clickMask)) {
+            clickPosition = hit.point;
+        }
+
+        portalPreview.transform.position = clickPosition;
+    }
+
+    void placePortal() {
+        if (Input.GetMouseButtonDown(0)) {
+            // place left portal
+            leftPortal.SetActive(true);
+            leftPortal.transform.position = clickPosition;
+        }
+
+        if (Input.GetMouseButtonDown(1)) {
+            // place right portal
+            rightPortal.SetActive(true);
+            rightPortal.transform.position = clickPosition;
+        }
+    }
+}
--- Brackeys Game Jam 2020.1/Assets/Scripts/Tank.cs
+++ Brackeys Game Jam 2020.1/Assets/Scripts/Tank.cs
@@ -11,6 +11,14 @@ public class Tank : MonoBehaviour
     private List<Vector3> waypoints;
     private int currentWaypoint;
     private Rigidbody rb;
+    private GameObject leftPortal, rightPortal, lastPortal;
+    private bool teleportable = true;
+
+    private void Awake() {
+        // get portals
+        leftPortal = GameObject.Find("LeftPortal");
+        rightPortal = GameObject.Find("RightPortal");
+    }
 
     void Start()
     {
@@ -54,10 +62,37 @@ public class Tank : MonoBehaviour
     }
 
     private void OnTriggerEnter(Collider other) {
+        // entering normal waypoint
         if (other.gameObject.tag == "Waypoint") {
             currentWaypoint++;
             if (currentWaypoint >= waypoints.Count)
                 currentWaypoint = 0;
         }
+
+        // entering last waypoint
+        if (other.gameObject.tag == "FinalWaypoint") {
+            // TODO: Won game
+        }
+
+        // entering portal
+        if (other.gameObject.tag == "Portal" && teleportable) {
+            if (other.gameObject.name == "LeftPortal") {
+                // teleport to right portal
+                transform.position = rightPortal.transform.position;
+                lastPortal = leftPortal;
+            } else {
+                // teleport to left portal
+                transform.position = leftPortal.transform.position;
+                lastPortal = rightPortal;
+            }
+            teleportable = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.name == "LeftPortal" && lastPortal == rightPortal)
+            teleportable = true;
+        if (other.gameObject.name == "RightPortal" && lastPortal == leftPortal)
+            teleportable = true;
     }
 }
