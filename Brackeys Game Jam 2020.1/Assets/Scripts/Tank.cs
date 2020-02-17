b6bf7b1d668be850e404ee2e975477205125f1cd
3cki <ecki_cool@gmx.de>
2020-02-17 12:27:24 +0100

    basic tank movement working

--- Brackeys Game Jam 2020.1/Assets/Scripts/Tank.cs
+++ Brackeys Game Jam 2020.1/Assets/Scripts/Tank.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class Tank : MonoBehaviour
 {
     public GameObject track;
+    public GameObject turret;
+    public float speed;
 
-    private List<Vector2> waypoints;
+    private List<Vector3> waypoints;
+    private int currentWaypoint;
+    private Rigidbody rb;
 
     void Start()
     {
@@ -14,15 +18,46 @@ public class Tank : MonoBehaviour
         if (track == null)
             track = GameObject.Find("Track");
 
-        waypoints = new List<Vector2>();
+        waypoints = new List<Vector3>();
 
         foreach (Transform child in track.transform) {
-            waypoints.Add(new Vector2(child.position.x, child.position.z));
+            waypoints.Add(new Vector3(child.position.x, transform.position.y, child.position.z));
         }
+
+        // get rigidbody
+        rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        
+        LookAtWaypoint();
+        MoveTowardsWaypoint();
+        TurretLookAtWaypoint();
+    }
+
+    void LookAtWaypoint() {
+        Vector3 targetDirection = waypoints[currentWaypoint] - transform.position;
+        float singleStep = speed * Time.deltaTime / 10;
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+        transform.rotation = Quaternion.LookRotation(newDirection);
+    }
+
+    void TurretLookAtWaypoint() {
+        Vector3 targetDirection = waypoints[currentWaypoint] - transform.position;
+        float singleStep = speed * Time.deltaTime;
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, singleStep);
+        turret.transform.rotation = Quaternion.LookRotation(newDirection);
+    }
+
+    void MoveTowardsWaypoint() {
+        rb.velocity = (transform.forward * speed);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Waypoint") {
+            currentWaypoint++;
+            if (currentWaypoint >= waypoints.Count)
+                currentWaypoint = 0;
+        }
     }
 }
