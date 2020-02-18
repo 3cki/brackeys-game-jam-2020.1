8a51bfd6feaf32af67f315b69b172d6fd3310a20
3cki <eckertm2000@gmail.com>
2020-02-18 16:54:23 +0100

    added epic secon camera

--- Brackeys Game Jam 2020.1/Assets/Scripts/GameController.cs
+++ Brackeys Game Jam 2020.1/Assets/Scripts/GameController.cs
@@ -6,6 +6,7 @@ using TMPro;
 public class GameController : MonoBehaviour
 {
     public GameObject wonPanel, lostPanel, portalPlacer, timer, timeShown;
+    public Camera secondCamera;
 
     private float time;
 
@@ -15,11 +16,17 @@ public class GameController : MonoBehaviour
         time = timer.GetComponent<Timer>().time;
         Destroy(timer);
         timeShown.GetComponent<TextMeshProUGUI>().text = time.ToString() + "s";
+        Camera.main.enabled = false;
+        secondCamera.gameObject.SetActive(true);
+        secondCamera.enabled = true;
     }
 
     public void Lose() {
         lostPanel.SetActive(true);
         portalPlacer.GetComponent<PortalPlacement>().allowedToPlace = false;
         Destroy(timer);
+        Camera.main.enabled = false;
+        secondCamera.gameObject.SetActive(true);
+        secondCamera.enabled = true;
     }
 }
---
+++ Brackeys Game Jam 2020.1/Assets/Scripts/TankCamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankCamera : MonoBehaviour
+{
+    public float speed;
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, speed * Time.deltaTime, 0));
+    }
+}
