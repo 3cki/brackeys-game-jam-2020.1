e31697d25261ec1da4a193dfd465b54b232831bf
3cki <eckertm2000@gmail.com>
2020-02-18 16:35:29 +0100

    added timer

--- Brackeys Game Jam 2020.1/Assets/Scripts/GameController.cs
+++ Brackeys Game Jam 2020.1/Assets/Scripts/GameController.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject wonPanel, lostPanel, portalPlacer;
+    public GameObject wonPanel, lostPanel, portalPlacer, timer, timeShown;
+
+    private float time;
 
     public void Win() {
         wonPanel.SetActive(true);
         portalPlacer.GetComponent<PortalPlacement>().allowedToPlace = false;
+        time = timer.GetComponent<Timer>().time;
+        Destroy(timer);
+        timeShown.GetComponent<TextMeshProUGUI>().text = time.ToString() + "s";
     }
 
     public void Lose() {
         lostPanel.SetActive(true);
         portalPlacer.GetComponent<PortalPlacement>().allowedToPlace = false;
+        Destroy(timer);
     }
 }
---
+++ Brackeys Game Jam 2020.1/Assets/Scripts/Timer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public float time = 0;
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        this.GetComponent<TextMeshProUGUI>().text = time.ToString();
+    }
+}
