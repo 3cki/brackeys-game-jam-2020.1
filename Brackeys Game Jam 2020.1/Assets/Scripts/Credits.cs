3f9547725bb57f3b8a5df7480d5939ed4661d180
3cki <eckertm2000@gmail.com>
2020-02-19 20:09:40 +0100

    added credits

---
+++ Brackeys Game Jam 2020.1/Assets/Scripts/Credits.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+    public void LoadMenu() {
+        StartCoroutine(Menu());
+    }
+
+    IEnumerator Menu() {
+        yield return new WaitForSeconds(10);
+        SceneManager.LoadScene("Menu");
+    }
+}
--- Brackeys Game Jam 2020.1/Assets/Scripts/MenuButtons.cs
+++ Brackeys Game Jam 2020.1/Assets/Scripts/MenuButtons.cs
@@ -10,7 +10,7 @@ public class MenuButtons : MonoBehaviour
     }
 
     public void Credits() {
-
+        SceneManager.LoadScene("Credits");
     }
 
     public void QuitGame() {
